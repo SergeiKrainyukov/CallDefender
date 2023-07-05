9937a27cc2ae2705b558e3de9358a626774693d8
Sergei <sk_developer@mail.ru>
2023-07-05 15:58:01 +0300

    Top app bar added

--- app/src/main/java/com/example/calldefender/MainActivity.kt
+++ app/src/main/java/com/example/calldefender/MainActivity.kt
@@ -2,23 +2,48 @@ package com.example.calldefender
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.calldefender.common.PermissionControllerImpl
 
 class MainActivity : AppCompatActivity() {
 
     private val permissionsController = PermissionControllerImpl()
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
         permissionsController.registerActivityForRequestPermissions(this)
         permissionsController.requestDialerPermission(this)
+
+        drawerLayout = findViewById(R.id.my_drawer_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         permissionsController.handleDialerPermissionResult(this, requestCode, resultCode)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_search -> {
+            true
+        }
+        else -> {
+            if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+                true
+            } else super.onOptionsItemSelected(item)
+        }
     }
 }
