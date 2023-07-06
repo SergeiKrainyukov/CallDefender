92a5990942cc0d75dc280de40312b2b2e28a1568
Sergei <sk_developer@mail.ru>
2023-07-06 09:55:51 +0300

    MainFragment added

--- app/src/main/java/com/example/calldefender/ui/MainActivity.kt
+++ app/src/main/java/com/example/calldefender/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.calldefender
+package com.example.calldefender.ui
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,6 +6,7 @@ import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.calldefender.R
 import com.example.calldefender.common.PermissionControllerImpl
 
 class MainActivity : AppCompatActivity() {
@@ -19,15 +20,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
-        permissionsController.registerActivityForRequestPermissions(this)
-        permissionsController.requestDialerPermission(this)
+        initDrawerLayout()
+        setPermissionsController()
+    }
 
+    private fun initDrawerLayout() {
         drawerLayout = findViewById(R.id.my_drawer_layout)
-        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun setPermissionsController() {
+        permissionsController.registerActivityForRequestPermissions(this)
+        permissionsController.requestDialerPermission(this)
     }
 
     @Deprecated("Deprecated in Java")
@@ -40,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         R.id.action_search -> {
             true
         }
+
         else -> {
             if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
                 true
---
+++ app/src/main/java/com/example/calldefender/ui/MainFragment.kt
@@ -0,0 +1,8 @@
+package com.example.calldefender.ui
+
+import androidx.fragment.app.Fragment
+import com.example.calldefender.R
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+}
