049de410bcfbf6e1e0344bb4dcb34c06e488fb25
Sergei <sk_developer@mail.ru>
2023-07-07 09:36:01 +0300

    Tabs Added

--- app/src/main/java/com/example/calldefender/ui/MainFragment.kt
+++ app/src/main/java/com/example/calldefender/ui/MainFragment.kt
@@ -1,8 +1,26 @@
 package com.example.calldefender.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.calldefender.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment : Fragment(R.layout.fragment_main) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val data = listOf("data1", "data2")
+        val adapter = ViewPagerAdapter(data)
+        val viewPager = view.findViewById<ViewPager2>(R.id.view_pager)
+        val tabLayout = view.findViewById<TabLayout>(R.id.tabLayout)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "Tab ${position + 1}"
+        }.attach()
+
+    }
 
 }
---
+++ app/src/main/java/com/example/calldefender/ui/ViewPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.calldefender.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.calldefender.R
+
+class ViewPagerAdapter(val data: List<String>) : RecyclerView.Adapter<ViewPagerViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+        return ViewPagerViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_view_pager, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+}
+
+class ViewPagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(text: String) {
+        itemView.findViewById<TextView>(R.id.text).text = text
+    }
+}
