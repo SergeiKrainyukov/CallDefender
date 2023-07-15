54f8c331ba974f6e5d4b5980605354b6f193ae7e
Sergei <sk_developer@mail.ru>
2023-07-15 13:53:48 +0300

    code cleanup

--- app/src/main/java/com/example/calldefender/ui/fragment/callsFragment/CallsFragment.kt
+++ app/src/main/java/com/example/calldefender/ui/fragment/callsFragment/CallsFragment.kt
@@ -1,6 +1,5 @@
 package com.example.calldefender.ui.fragment.callsFragment
 
-//import com.example.calldefender.di.DaggerMainComponent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
