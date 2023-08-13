c85ad3a6c0800713f64706d4f91b142fbbc35453
Sergei <sk_developer@mail.ru>
2023-08-13 23:13:20 +0300

    block unfamiliar setting completed

--- app/src/main/java/com/example/calldefender/service/CallDefenderCallScreeningService.kt
+++ app/src/main/java/com/example/calldefender/service/CallDefenderCallScreeningService.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.provider.ContactsContract
-import android.telecom.Call
+import android.telecom.Call.Details
 import android.telecom.CallScreeningService
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.calldefender.CallDefenderApp
@@ -14,8 +14,10 @@ import com.example.calldefender.common.formatToPattern
 import com.example.calldefender.common.parseCountryCode
 import com.example.calldefender.common.removeTelPrefix
 import com.example.calldefender.repository.CallsRepository
+import com.example.calldefender.repository.SettingsRepository
 import com.example.calldefender.ui.model.CallType
 import com.example.calldefender.ui.model.CallUi
+import com.example.calldefender.ui.model.SettingUI
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import java.util.Date
@@ -23,34 +25,31 @@ import javax.inject.Inject
 
 class CallDefenderCallScreeningService : CallScreeningService() {
     @Inject
-    lateinit var repository: CallsRepository
+    lateinit var callsRepository: CallsRepository
+
+    @Inject
+    lateinit var settingsRepository: SettingsRepository
 
     override fun onCreate() {
         super.onCreate()
         (application as CallDefenderApp).appComponent.inject(this)
     }
 
-    override fun onScreenCall(callDetails: Call.Details) {
+    override fun onScreenCall(callDetails: Details) {
         val phoneNumber = callDetails.handle.toString().removeTelPrefix().parseCountryCode()
-        var response = CallResponse.Builder()
-        response = handlePhoneCall(response, phoneNumber)
-
-        respondToCall(callDetails, response.build())
+        handlePhoneCall(callDetails, CallResponse.Builder(), phoneNumber)
     }
 
     private fun handlePhoneCall(
+        callDetails: Details,
         response: CallResponse.Builder,
         phoneNumber: String
-    ): CallResponse.Builder {
-        if (isInContacts(applicationContext, phoneNumber)) return response
-        response.apply {
-            setRejectCall(true)
-            setDisallowCall(true)
-            setSkipCallLog(false)
-            addCallToRepository(phoneNumber, CallType.REJECTED)
+    ) {
+        if (isInContacts(applicationContext, phoneNumber)) {
+            respondToCall(callDetails, response.build())
+            return
         }
-        updateUI()
-        return response
+        checkSettings(callDetails, response, phoneNumber)
     }
 
     @SuppressLint("Range")
@@ -77,8 +76,35 @@ class CallDefenderCallScreeningService : CallScreeningService() {
         return false
     }
 
+    @SuppressLint("CheckResult")
+    private fun checkSettings(
+        callDetails: Details,
+        response: CallResponse.Builder,
+        phoneNumber: String
+    ) {
+        settingsRepository.getAllSettings().subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                it.forEach {
+                    when (it) {
+                        is SettingUI.BlockUnfamiliarCallsSettingUI -> {
+                            if (it.isEnabled) {
+                                rejectCall(callDetails, response, phoneNumber)
+                                updateUI()
+                                return@subscribe
+                            }
+                        }
+
+                        else -> return@subscribe
+                    }
+                }
+            }, {
+                it.printStackTrace()
+            })
+    }
+
     private fun addCallToRepository(callNumber: String, callType: CallType) {
-        repository.addCall(
+        callsRepository.addCall(
             CallUi(
                 callNumber,
                 Date().formatToPattern(DatePatterns.DEFAULT.pattern),
@@ -87,6 +113,20 @@ class CallDefenderCallScreeningService : CallScreeningService() {
         ).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe()
+    }
+
+    private fun rejectCall(
+        callDetails: Details,
+        response: CallResponse.Builder,
+        phoneNumber: String
+    ) {
+        val callResponse = response.apply {
+            setRejectCall(true)
+            setDisallowCall(true)
+            setSkipCallLog(false)
+        }.build()
+        respondToCall(callDetails, callResponse)
+        addCallToRepository(phoneNumber, CallType.REJECTED)
     }
 
     private fun updateUI() {
